3e856b637a700dc09fd806d02df094b382a21cc4
kwader <kw@de.r>
2026-02-22 14:55:33 +0100

    yes

---
+++ go.mod
@@ -0,0 +1,50 @@
+module github.com/hectorgimenez/koolo
+
+go 1.23
+
+require (
+	github.com/billgraziano/dpapi v0.5.0
+	github.com/bwmarrin/discordgo v0.28.1
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gorilla/websocket v1.5.3
+	github.com/hectorgimenez/d2go v0.0.0-20250314185000-169a76515285
+	github.com/inkeliz/gowebview v1.0.1
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
+	github.com/otiai10/copy v1.14.0
+	golang.ngrok.com/ngrok v1.13.0
+	golang.org/x/sync v0.10.0
+	golang.org/x/sys v0.28.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.40.0 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.9.0 // indirect
+)
+
+require (
+	git.wow.st/gmp/jni v0.0.0-20210610011705-34026c7e22d0 // indirect
+	github.com/expr-lang/expr v1.16.9 // indirect
+	github.com/go-rod/rod v0.116.2
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/inconshreveable/log15 v3.0.0-testing.5+incompatible // indirect
+	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
+	github.com/inkeliz/w32 v1.0.2 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.ngrok.com/muxado/v2 v2.0.1 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+replace github.com/hectorgimenez/d2go => github.com/kwader2k/d2go v0.0.0-20260220000340-a49f6eff4f5b
---
+++ internal/game/mouse.go
@@ -0,0 +1,65 @@
+package game
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/lxn/win"
+)
+
+const (
+	RightButton MouseButton = win.MK_RBUTTON
+	LeftButton  MouseButton = win.MK_LBUTTON
+
+	ShiftKey ModifierKey = win.VK_SHIFT
+	CtrlKey  ModifierKey = win.VK_CONTROL
+)
+
+type MouseButton uint
+type ModifierKey byte
+
+const pointerReleaseDelay = 150 * time.Millisecond
+
+// MovePointer moves the mouse to the requested position, x and y should be the final position based on
+// pixels shown in the screen. Top-left corner is 0,0
+func (hid *HID) MovePointer(x, y int) {
+	hid.gr.updateWindowPositionData()
+	x = hid.gr.WindowLeftX + x
+	y = hid.gr.WindowTopY + y
+
+	hid.gi.CursorPos(x, y)
+	lParam := calculateLparam(x, y)
+	win.SendMessage(hid.gr.HWND, win.WM_NCHITTEST, 0, lParam)
+	win.SendMessage(hid.gr.HWND, win.WM_SETCURSOR, 0x000105A8, 0x2010001)
+	win.PostMessage(hid.gr.HWND, win.WM_MOUSEMOVE, 0, lParam)
+}
+
+// Click just does a single mouse click at current pointer position
+func (hid *HID) Click(btn MouseButton, x, y int) {
+	hid.MovePointer(x, y)
+	x = hid.gr.WindowLeftX + x
+	y = hid.gr.WindowTopY + y
+
+	lParam := calculateLparam(x, y)
+	buttonDown := uint32(win.WM_LBUTTONDOWN)
+	buttonUp := uint32(win.WM_LBUTTONUP)
+	if btn == RightButton {
+		buttonDown = win.WM_RBUTTONDOWN
+		buttonUp = win.WM_RBUTTONUP
+	}
+
+	win.SendMessage(hid.gr.HWND, buttonDown, 1, lParam)
+	sleepTime := rand.Intn(keyPressMaxTime-keyPressMinTime) + keyPressMinTime
+	time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+	win.SendMessage(hid.gr.HWND, buttonUp, 1, lParam)
+}
+
+func (hid *HID) ClickWithModifier(btn MouseButton, x, y int, modifier ModifierKey) {
+	hid.gi.OverrideGetKeyState(byte(modifier))
+	hid.Click(btn, x, y)
+	hid.gi.RestoreGetKeyState()
+}
+
+func calculateLparam(x, y int) uintptr {
+	return uintptr(y<<16 | x)
+}
