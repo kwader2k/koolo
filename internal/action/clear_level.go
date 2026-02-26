1f664b419216c63347bac59b147e3bf4afbd4374
binary-code-labs <daniel@916labs.com>
2026-02-25 21:16:10 -0800

    clean up global override

--- internal/action/clear_level.go
+++ internal/action/clear_level.go
@@ -35,9 +35,6 @@ func ClearCurrentLevelEx(openChests bool, filter data.MonsterFilter, shouldInter
 
 	openAllChests := ctx.CharacterCfg.Game.InteractWithChests || openChests
 	openSuperOnly := ctx.CharacterCfg.Game.InteractWithSuperChests && !openAllChests
-	if !openAllChests && !openSuperOnly {
-		openChests = false
-	}
 
 	// We can make this configurable later, but 20 is a good starting radius.
 	const pickupRadius = 20
